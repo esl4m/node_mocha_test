5c1a69501cd5fcf3d364125cb5cd1017a30df9d1
Eslam Elgamal <eslam.elgamal@ibm.com>
2018-01-16 00:10:14 +0200

    Adds test function to return zero if no value in array input

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect;
 var assert = require('assert');
+
+var sum = require('../src/test-add');
 
 beforeEach(function() {
     console.log('before every test in every file');
@@ -8,6 +12,13 @@ describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+
+    describe('Function tests', function() {
+        it('should return zero for a zero length input array', function () {
+            getSum = new sum([]);
+            expect(getSum.getTotal()).to.equal(0);
         });
     });
 });
