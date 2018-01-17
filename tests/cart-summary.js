4e81b80f50b4172239a008dc6dd3e92cb13c0ba1
Eslam Elgamal <eslam.elgamal@ibm.com>
2018-01-17 17:07:44 +0200

    Fixes cart summary

--- tests/cart-summary.js
+++ tests/cart-summary.js
@@ -15,22 +15,10 @@ beforeEach(function() {
 
 describe('CartSummary', function () {
     it('getSubtotal() should return the sum of the price * quantity for all items', function () {
-            cartSummary = new CartSummary([
-            // {
-            //     id: 1,
-            //     quantity: 4,
-            //     price: 50
-            // },
-            // {
-            //     id: 2,
-            //     quantity: 2,
-            //     price: 30
-            // },
-            // {
-            //     id: 3,
-            //     quantity: 1,
-            //     price: 40
-            // }
+            var cartSummary = new CartSummary([
+                {id: 1, quantity: 4, price: 50},
+                {id: 2, quantity: 2, price: 30},
+                {id: 3, quantity: 1, price: 40}
             ]);
             console.log(cartSummary.getSubtotal());
             expect(cartSummary.getSubtotal()).to.equal(300);
